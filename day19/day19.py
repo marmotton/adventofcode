3915392b46b5afb3ee3907d8025c84c6f44664b1
Benjamin Graf <benjamin@eende.net>
2020-12-25 16:38:13 +0100

    Day 19 part 2

--- day19/day19.py
+++ day19/day19.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 rules = {}
 messages = []
@@ -62,8 +63,8 @@ def generate_messages(rule_no):
 
     return messages
 
-
-valid_messages = generate_messages(0)
+t0 = time.time()
+valid_messages = set(generate_messages(0))
 
 n_valid_messages = 0
 
@@ -71,8 +72,66 @@ for message in messages:
     if message in valid_messages:
         n_valid_messages += 1
 
-print("Part 1: {} valid messages".format(n_valid_messages))
+t1 = time.time()
+print("Part 1: {} valid messages. Verification time {:.2f}s".format(n_valid_messages, t1 - t0))
+
+# Using regex
+def generate_regex(rule_no):
+    if isinstance(rules[rule_no][0], str):
+        regex = rules[rule_no][0]
+
+    else:
+        if len(rules[rule_no]) > 1:
+            regex = "("
+        else:
+            regex = ""
+
+        # Generate all parts of the OR
+        for rule in rules[rule_no]:
+            for num in rule:
+                regex += generate_regex(num)
+            regex += "|"
+
+        # Remove the last |
+        regex = regex[:-1]
+
+        if len(rules[rule_no]) > 1:
+            regex += ")"
+
+    return regex
+
+t2 = time.time()
+
+n_valid_messages = 0
+big_regex = re.compile(generate_regex(0))
+for message in messages:
+    if big_regex.fullmatch(message):
+        n_valid_messages += 1
+
+t3 = time.time()
+
+print("Part 1 using regex: {} valid messages. Verification time {:.2f}s".format(n_valid_messages, t3 - t2))
+
 
 # Part 2
-rules[8] = [[42], [42, 8]]
-rules[11] = [[42, 31], [42, 11, 31]]
+# Instead of an infinite loop, generate some more options for rules 8 and 11.
+# The upper value of the range() is set by 
+for i in range(2, 10):
+    rules[8].append([42] * i)
+
+for i in range(2, 10):
+    rules[11].append([42] * i + [31] * i)
+
+
+t4 = time.time()
+
+n_valid_messages = 0
+big_regex = re.compile(generate_regex(0))
+
+for message in messages:
+    if big_regex.fullmatch(message):
+        n_valid_messages += 1
+
+t5 = time.time()
+
+print("Part 2 using regex: {} valid messages. Verification time {:.2f}s".format(n_valid_messages, t5 - t4))
